091bece174aec5670ecbf22cc5ffa8dd7ac28dce
pc <rafastb@gmail.com>
2022-06-02 02:35:17 -0300

    Update

--- PrimeiroGit.java
+++ PrimeiroGit.java
@@ -1,9 +1,6 @@
 public class PrimeiroGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        Souldlkasflasf,l;amfa
-            flknsandflkan;f,as
-                fmsnlkfnsdlfn
 
     }
 }
